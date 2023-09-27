8915b39852e0d621c44e317d1e7858112b4f4dea
yachelovek1337 <145943519+yachelovek1337@users.noreply.github.com>
2023-09-27 15:43:53 +0300

    Fix BackupCleaner
    Fix BackupCleaner

--- src/backup/BackupCleaner.java
+++ src/backup/BackupCleaner.java
@@ -3,7 +3,9 @@ package backup;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.attribute.BasicFileAttributes;
+import java.nio.file.attribute.FileTime;
 import java.util.Arrays;
 
 public class BackupCleaner {
@@ -15,28 +17,54 @@ public class BackupCleaner {
     }
 
     public void cleanOldBackups() {
-        File backupDir = new File(backupDirectory);
-        File[] backupFiles = backupDir.listFiles();
+        try {
+            File backupDir = new File(backupDirectory);
+            File[] backupFiles = backupDir.listFiles();
 
-        if (backupFiles != null) {
-            Arrays.stream(backupFiles)
-                    .filter(this::isOldBackup)
-                    .forEach(File::delete);
+            if (backupFiles != null) {
+                for (File file : backupFiles) {
+                    if (isOldBackup(file.toPath())) {
+                        if (file.isDirectory()) {
+                            deleteDirectory(file);
+                        } else {
+                            Files.delete(file.toPath());
+                        }
+                    }
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
 
-    private boolean isOldBackup(File backupFile) {
-        BasicFileAttributes attrs = null;
+    private static void deleteDirectory(File directory) throws IOException {
+        File[] files = directory.listFiles();
+
+        if (files != null) {
+            for (File file : files) {
+                if (file.isDirectory()) {
+                    deleteDirectory(file);
+                } else {
+                    Files.delete(file.toPath());
+                }
+            }
+        }
+
+        Files.delete(directory.toPath());
+    }
+
+    private static boolean isOldBackup(Path backupPath) {
         try {
-            attrs = Files.readAttributes(backupFile.toPath(), BasicFileAttributes.class);
+            BasicFileAttributes attrs = Files.readAttributes(backupPath, BasicFileAttributes.class);
+            FileTime createTime = attrs.creationTime();
+            long currentTimeMillis = System.currentTimeMillis();
+            long oneDayInMillis = 24 * 60 * 60 * 1000; // 1 день в миллисекундах
+
+            return createTime.toMillis() < currentTimeMillis - oneDayInMillis;
         } catch (IOException e) {
             e.printStackTrace();
+            return false;
         }
-        long creationTimeMillis = attrs.creationTime().toMillis();
-        long currentTimeMillis = System.currentTimeMillis();
-        long oneDayInMillis = 24 * 60 * 60 * 1000;
-
-        return currentTimeMillis - creationTimeMillis > oneDayInMillis;
     }
 
 }
